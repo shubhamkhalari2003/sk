68a55e941116a5940bf4609095f89a5c908e5bcf
Shubham kumar <93025079+shubhamkhalari2003@users.noreply.github.com>
2024-07-19 07:29:18 +0530

    latest update

---
+++ typedScript.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["a Student","a Software Engineer","a Programmer", "a Web Developer", "a Tech Enthusiat"],
+  typeSpeed: 80,
+  backSpeed: 80,
+  smartBackspace: true,
+  loop: true
+});
